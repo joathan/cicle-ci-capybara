3aecce2fe125eef126b943b461ba40209179dd11
Joathan Souza <joathanf@gmail.com>
2019-12-12 16:02:34 -0300

    add spec edit state

--- spec/features/state_spec.rb
+++ spec/features/state_spec.rb
@@ -6,10 +6,23 @@ RSpec.describe 'State', type: :feature do
     expect(page).to have_content('Hello, world!')
   end
 
-  scenario '' do
+  scenario 'create a new state' do
     visit 'states/new'
     fill_in 'Name', with: 'Alagoas'
     click_button 'Create State'
     expect(page).to have_content 'State was successfully created'
+  end
+
+  scenario 'edit a state' do
+    visit 'states/new'
+    fill_in 'Name', with: 'Alagoas'
+    click_button 'Create State'
+
+    state = State.last
+
+    visit "#{state.id}/edit"
+    fill_in 'Name', with: 'Alagoass'
+    click_button 'Update State'
+    expect(page).to have_content 'State was successfully updated.'
   end
 end
